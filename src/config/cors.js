69d6c11982ef62263479f4ddb37335fa58113a29
Jonhatan <jonyvilla44@gmail.com>
2025-02-27 17:47:56 -0600

    cors configuration

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,13 +1,15 @@
 // CORS configuration
 const corsOptions = {
-  origin: function(origin, callback) {
-    const url = [process.env.FRONTEND_URL];
-    if (url.includes(origin)) {
+  origin: function (origin, callback) {
+    const allowedOrigins = [process.env.FRONTEND_URL];
+
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
+  credentials: true // Si usas cookies o autenticación con tokens
 };
 
 export default corsOptions;
